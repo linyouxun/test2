5dd991e81bd4046eaafef635982a23e00bfc27d2
lyx <724040735@qq.com>
2014-10-23 19:13:27 +0800

    背包-动归

---
+++ package/main.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+#define MAXSIZE 1000
+int f[MAXSIZE+1],c[MAXSIZE+1],w[MAXSIZE+1];
+int main(int argc,char *argv[])
+{
+	int N,V;
+	cin>>N>>V;
+	int i=1;
+	for(;i<=N;++i)
+	{
+		cin>>c[i]>>w[i];
+	}
+	for(i=1;i<=N;++i)
+	{
+		for(int v=V;v>=c[i];--v)//c[i]可优化为bound,bound=max{V-sumc[i,...n],c[i]}
+		{
+			f[v]=(f[v]>f[v-c[i]]+w[i])?f[v]:f[v-c[i]]+w[i];
+		}
+	}
+	//当i=N时，可以跳出循环单独计算F[V]
+	cout<<f[V]<<'\n';
+	system("pause");
+	return 0;
+}
